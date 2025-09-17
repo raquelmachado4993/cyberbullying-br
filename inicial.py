ed40e6791fb04de7ab519fa33e70ee3eb83d0e66
raquelmachado4993@gmail.com <@Hellvys1>
2025-09-17 18:22:10 -0300

    update

--- inicial.py
+++ inicial.py
@@ -18,7 +18,7 @@ def tokenizer_emoji_aware(text):
     text = fun.separar_emojis(text)
     return text.split()
 
-#Fun√ß√£o pra limpar o que o usu√°rio digitou da primeira vez
+#FunÁ„o pra limpar o que o usu·rio digitou da primeira vez
 def clear_text():
     st.session_state["texto"] = '' 
     alert.empty()
@@ -26,8 +26,8 @@ def clear_text():
 # Menu lateral colorido
 with st.sidebar:
     selected = option_menu(
-        menu_title="Op√ß√µes",
-        options=["Home", "Aplica√ß√£o", "Recursos", "Contatos", "Refer√™ncias", "M√©tricas"],
+        menu_title="OpÁıes",
+        options=["Home", "AplicaÁ„o", "Recursos", "Contatos", "ReferÍncias", "MÈtricas"],
         icons=["house-heart-fill", "code-slash", "folder-fill", "person-lines-fill", "bookmark-heart", "file-bar-graph"],
         default_index=0,
         styles={
@@ -38,21 +38,21 @@ with st.sidebar:
         }
     )
 
-#Isso aqui mostra o menu com as op√ß√µes #outro modelo de menu
+#Isso aqui mostra o menu com as opÁıes #outro modelo de menu
 #selected = option_menu(
 #    menu_title=None,
-#    options = ["Home_page", "Aplica√ß√£o", "Recursos","Contatos","Refer√™ncias","M√©tricas"],
+#    options = ["Home_page", "AplicaÁ„o", "Recursos","Contatos","ReferÍncias","MÈtricas"],
 #    icons = ["house-heart-fill","code-slash","folder-fill","person-lines-fill","bookmark-heart","bookmark-heart"],
 #    default_index=0,
 #   orientation="horizontal",
 #   )
 
-#Cada if √© o conte√∫do de um menu 
-if selected =="Refer√™ncias":
-    st.title("Refer√™ncias")
+#Cada if È o conte˙do de um menu 
+if selected =="ReferÍncias":
+    st.title("ReferÍncias")
     st.markdown(""" 
     
-    * <p style="text-align:justify; font-size: 18px;"> <i>Text Mining for Cyberbullying Detection: a Brazilian Portuguese Evaluation</i> (2021) - Autores: Carolina Eberhart, Luciano Ignaczak, M√°rcio Garcia Martins - <a href="https://sol.sbc.org.br/index.php/stil/article/view/17788"> Acesse aqui </a></p>
+    * <p style="text-align:justify; font-size: 18px;"> <i>Text Mining for Cyberbullying Detection: a Brazilian Portuguese Evaluation</i> (2021) - Autores: Carolina Eberhart, Luciano Ignaczak, M·rcio Garcia Martins - <a href="https://sol.sbc.org.br/index.php/stil/article/view/17788"> Acesse aqui </a></p>
     
     * <p style="text-align:justify; font-size: 18px;"><i>Cyberbullying Detection in Social Networks: A Comparison Between Machine Learning and Transfer Learning Approaches </i> (2023)- </i> Autores: Teoh Wai Teng and Kasturi Dewi Varathan - <a href="https://ieeexplore.ieee.org/document/10122521"> Acesse aqui </a></p>
    
@@ -62,62 +62,62 @@ if selected =="Refer√™ncias":
 
 
 if selected == "Home":
-    st.title("Detec√ß√£o de cyberbullying em Portugu√™s Brasileiro  &#127463;&#127479; &#128187;")
+    st.title("DetecÁ„o de cyberbullying em PortuguÍs Brasileiro  &#127463;&#127479; &#128187;")
 
     
     st.markdown("""   
     
-    <p style="text-align:justify; font-size: 18px;">Esta aplica√ß√£o tem como objetivo auxiliar na amplia√ß√£o dos estudos em <strong>detec√ß√£o autom√°tica de mensagens com potencial de <i>cyberbullying</i></strong> em portugu√™s brasileiro investigando o uso de t√©cnicas de <strong> aprendizado de m√°quina <i>(machine learning)</i></strong>. </p>
+    <p style="text-align:justify; font-size: 18px;">Esta aplicaÁ„o tem como objetivo auxiliar na ampliaÁ„o dos estudos em <strong>detecÁ„o autom·tica de mensagens com potencial de <i>cyberbullying</i></strong> em portuguÍs brasileiro investigando o uso de tÈcnicas de <strong> aprendizado de m·quina <i>(machine learning)</i></strong>. </p>
 
-    <p style="text-align:justify ; font-size: 18px;">A aplica√ß√£o √© um prot√≥tipo, isto √©, uma pequena amostra como parte de uma <strong>pesquisa de Doutorado</strong> em Inform√°tica que investiga a exposi√ß√£o de crian√ßas e adolescentes a amea√ßas nos ambientes digitais e prop√µe o uso de Intelig√™ncia Artificial para detect√°-las e mitig√°-las. 
+    <p style="text-align:justify ; font-size: 18px;">A aplicaÁ„o È um protÛtipo, isto È, uma pequena amostra como parte de uma <strong>pesquisa de Doutorado</strong> em Inform·tica que investiga a exposiÁ„o de crianÁas e adolescentes a ameaÁas nos ambientes digitais e propıe o uso de InteligÍncia Artificial para detect·-las e mitig·-las. 
     
       
     """, unsafe_allow_html=True)
 
     st.caption("""<h2><font color="ff1493">Como funciona? &#128289; </font></h1> """, unsafe_allow_html=True)
     st.markdown("""   
-    <p style="text-align:justify; font-size: 18px;"> Ao digitar uma frase e envi√°-la para an√°lise, o sistema verifica se ela tem <strong>caracter√≠sticas similares a mensagens de <i>cyberbullying</i></strong> , 
+    <p style="text-align:justify; font-size: 18px;"> Ao digitar uma frase e envi·-la para an·lise, o sistema verifica se ela tem <strong>caracterÌsticas similares a mensagens de <i>cyberbullying</i></strong> , 
     com base em modelos treinados com dados reais. </p> """, unsafe_allow_html=True)
 
 
     st.caption("""<h2><font color="ff1493"> SVM x Naive Bayes &#128202; </font></h1>""", unsafe_allow_html=True)
     st.markdown("""   
     <p style="text-align:justify; font-size: 18px;"> 
-    Nesta aplica√ß√£o, utilizamos dois algoritmos de aprendizado de m√°quina: <i>Naive Bayes</i> e SVM <i>(Support vector machine)</i>. 
-    Ambos s√£o capazes de aprender com exemplos previamente classificados para identificar se uma frase pode ser considerada <i>cyberbullying</i> ou n√£o, 
+    Nesta aplicaÁ„o, utilizamos dois algoritmos de aprendizado de m·quina: <i>Naive Bayes</i> e SVM <i>(Support vector machine)</i>. 
+    Ambos s„o capazes de aprender com exemplos previamente classificados para identificar se uma frase pode ser considerada <i>cyberbullying</i> ou n„o, 
     mas adotam abordagens diferentes. </p>
 
 
-    <p style="text-align:justify; font-size: 18px;"> O SVM considera o padr√£o geral da frase, buscando separar frases ofensivas e n√£o ofensivas com base em limites definidos 
-    durante o treinamento. J√°, o Naive Bayes se baseia na frequ√™ncia das palavras e estima a probabilidade de uma frase ser ofensiva  
-    ou n√£o com base nas palavras que ela cont√©m. </p>
+    <p style="text-align:justify; font-size: 18px;"> O SVM considera o padr„o geral da frase, buscando separar frases ofensivas e n„o ofensivas com base em limites definidos 
+    durante o treinamento. J·, o Naive Bayes se baseia na frequÍncia das palavras e estima a probabilidade de uma frase ser ofensiva  
+    ou n„o com base nas palavras que ela contÈm. </p>
 
     <p style="text-align:justify; font-size: 18px;"> Em frases curtas com termos mais agressivos, ambos os algoritmos costumam concordar. Em frases mais sutis ou com ironia, o SVM pode 
-    oferecer uma an√°lise um pouco mais precisa. </p> """, unsafe_allow_html=True)
+    oferecer uma an·lise um pouco mais precisa. </p> """, unsafe_allow_html=True)
 
     st.caption("""<h2><font color="ff1493">Como usar? &#128220</font></h1>""", unsafe_allow_html=True)
     st.markdown("""
-    <p style="text-align:justify; font-size: 18px;"> Acesse a aba <strong>App</strong>, digite uma frase no campo indicado e selecione o modelo de classifica√ß√£o desejado. Clique no bot√£o "Analisar".
-    A resposta ser√° exibida em poucos segundos.</p>
+    <p style="text-align:justify; font-size: 18px;"> Acesse a aba <strong>App</strong>, digite uma frase no campo indicado e selecione o modelo de classificaÁ„o desejado. Clique no bot„o "Analisar".
+    A resposta ser· exibida em poucos segundos.</p>
 
-    <p style="text-align:justify; font-size: 18px;"> Mais informa√ß√µes sobre o conjunto de dados utilizado na etapa de treinamento do modelo est√£o dispon√≠veis no menu <strong>Recursos</strong>. </p> """, unsafe_allow_html=True)
+    <p style="text-align:justify; font-size: 18px;"> Mais informaÁıes sobre o conjunto de dados utilizado na etapa de treinamento do modelo est„o disponÌveis no menu <strong>Recursos</strong>. </p> """, unsafe_allow_html=True)
 
     st.caption("""<h2><font color="ff1493">Importante </font>   <font color="ffd700"> &#9888;</font>   </h1>""", unsafe_allow_html=True)
     st.markdown("""
-    <p style="text-align:justify; font-size: 18px;"> Esta ferramenta √© apenas um <strong>suporte √† detec√ß√£o</strong>. A determina√ß√£o de um caso de cyberbullying envolve outros fatores importantes,
-     como <strong>contexto</strong>, <strong>recorr√™ncia</strong> e <strong>inten√ß√£o</strong>. A an√°lise autom√°tica n√£o substitui a 
-     avalia√ß√£o humana.</p> """, unsafe_allow_html=True)
+    <p style="text-align:justify; font-size: 18px;"> Esta ferramenta È apenas um <strong>suporte ‡ detecÁ„o</strong>. A determinaÁ„o de um caso de cyberbullying envolve outros fatores importantes,
+     como <strong>contexto</strong>, <strong>recorrÍncia</strong> e <strong>intenÁ„o</strong>. A an·lise autom·tica n„o substitui a 
+     avaliaÁ„o humana.</p> """, unsafe_allow_html=True)
                
     st.caption("""<h2><font color="ff1493">Trabalhos futuros &#128679; </font></h2>""", unsafe_allow_html=True)
     st.markdown("""   
-    <p style="text-align:justify; font-size: 18px;"> Em vers√µes futuras, a aplica√ß√£o dever√° ser aprimorada e adaptada para funcionar como uma <strong>extens√£o
-     de navegador</strong> e/ou um <strong>aplicativo</strong>, permitindo a detec√ß√£o em tempo real e envio de notifica√ß√µes aos usu√°rios 
-     incentivando a reflex√£o antes de publicar mensagens ofensivas. Al√©m disso, o <i>dataset</i> tamb√©m deve ser aprimorado para incluir exemplos mais diversos e representativos,
+    <p style="text-align:justify; font-size: 18px;"> Em versıes futuras, a aplicaÁ„o dever· ser aprimorada e adaptada para funcionar como uma <strong>extens„o
+     de navegador</strong> e/ou um <strong>aplicativo</strong>, permitindo a detecÁ„o em tempo real e envio de notificaÁıes aos usu·rios 
+     incentivando a reflex„o antes de publicar mensagens ofensivas. AlÈm disso, o <i>dataset</i> tambÈm deve ser aprimorado para incluir exemplos mais diversos e representativos,
      incluindo textos advindos de outras plataformas. </p> """, unsafe_allow_html=True)
 
-if selected == "M√©tricas":
+if selected == "MÈtricas":
 
-    st.caption("""<h2><font color="ff1493"> M√©tricas do modelo treinado com Naive Bayes </font></h1>""", unsafe_allow_html=True)
+    st.caption("""<h2><font color="ff1493"> MÈtricas do modelo treinado com Naive Bayes </font></h1>""", unsafe_allow_html=True)
     
     spamreader = fun.ler("rel_class_nb.txt")
     
@@ -127,7 +127,7 @@ if selected == "M√©tricas":
     
     spamreader = fun.ler("rel_class_svm.txt")
     
-    st.caption("""<h2><font color="ff1493"> M√©tricas do modelo treinado com SVM </font></h1>""", unsafe_allow_html=True)
+    st.caption("""<h2><font color="ff1493"> MÈtricas do modelo treinado com SVM </font></h1>""", unsafe_allow_html=True)
     
 
     for row in spamreader:
@@ -139,7 +139,7 @@ if selected == "Recursos":
     st.markdown(""" 
                 
                 
-                ***C√≥digo fonte***
+                ***CÛdigo fonte***
                 >  https://github.com/raquelmachado4993/cyberbullying-br 
                 ---
                 ***Dataset***
@@ -153,20 +153,20 @@ if selected =="Contatos":
                 
                 
                 #### Professora Ma. Raquel M. M. Fernandes
-                ##### ***Doutoranda em Inform√°tica, Programa de P√≥s-Gradua√ß√£o em Inform√°tica (PPGI) da Universidade Federal do Rio de Janeiro (UFRJ)***
+                ##### ***Doutoranda em Inform·tica, Programa de PÛs-GraduaÁ„o em Inform·tica (PPGI) da Universidade Federal do Rio de Janeiro (UFRJ)***
                 
                 Email: raquel.fernandes@ufrj.br
                                
                 ---
                 #### Professor Dr. Luiz Fernando Rust da Costa Carmo
-                ##### ***Programa de P√≥s-Gradua√ß√£o em Inform√°tica (PPGI) da Universidade Federal do Rio de Janeiro (UFRJ)***
+                ##### ***Programa de PÛs-GraduaÁ„o em Inform·tica (PPGI) da Universidade Federal do Rio de Janeiro (UFRJ)***
                 
                 Email: rust@nce.ufrj.br
          
                 ---
 
                 #### Professora Dra. Claudia Lage Rebello da Motta
-                ##### ***Programa de P√≥s-Gradua√ß√£o em Inform√°tica (PPGI) da Universidade Federal do Rio de Janeiro (UFRJ)***
+                ##### ***Programa de PÛs-GraduaÁ„o em Inform·tica (PPGI) da Universidade Federal do Rio de Janeiro (UFRJ)***
                 
                 Email: claudiam@nce.ufrj.br
          
@@ -176,10 +176,10 @@ if selected =="Contatos":
 
     
  
-if selected == "Aplica√ß√£o":  
+if selected == "AplicaÁ„o":  
 
       
-# Disponibilizei o resultado do treinamento com SVM e NB, o usu√°rio pode escolher aqui 
+# Disponibilizei o resultado do treinamento com SVM e NB, o usu·rio pode escolher aqui 
 
   
    
@@ -194,7 +194,7 @@ if selected == "Aplica√ß√£o":
 
     
     
-    # CSS para bot√µes rosa
+    # CSS para botıes rosa
     st.markdown("""
     <style>
         .stButton > button {
@@ -226,8 +226,8 @@ if selected == "Aplica√ß√£o":
 
  
 
-# Pegando a frase que o usu√°rio vai digitar para analisar..
-    input_text = st.text_input('Digite uma frase para an√°lise:', key="texto")
+# Pegando a frase que o usu·rio vai digitar para analisar..
+    input_text = st.text_input('Digite uma frase para an·lise:', key="texto")
 
     
     #button = st.button("Analisar")
@@ -251,7 +251,7 @@ if selected == "Aplica√ß√£o":
 
     
 
-    #Dependendo da escolha, vai chamar uma fun√ß√£o ou outra
+    #Dependendo da escolha, vai chamar uma funÁ„o ou outra
 
         with st.spinner("Analisando..."):
             
@@ -260,7 +260,7 @@ if selected == "Aplica√ß√£o":
                 modelo = fun.carregarmodelo('finalized_svm_model.joblib')
                 vectorizer = fun.carregarmodelo('finalized_svm_vectorizer.joblib')
                 
-                # Mostra a resposta da an√°lise aqui 
+                # Mostra a resposta da an·lise aqui 
                 resp, flag = fun.classificar_frase_svm(modelo,vectorizer,input_text)
                 if(flag):
                     alert =st.warning(resp)
@@ -273,7 +273,7 @@ if selected == "Aplica√ß√£o":
                 modelo_nb = fun.carregarmodelo('finalized_nb_model.joblib')
                 vectorizer_nb = fun.carregarmodelo('finalized_nb_vectorizer.joblib')
                 
-                # Mostra a resposta da an√°lise aqui 
+                # Mostra a resposta da an·lise aqui 
                 resp, flag = fun.classificar_frase_nb(modelo_nb,vectorizer_nb,input_text)
                 if(flag):
                     alert =st.warning(resp)
@@ -288,7 +288,7 @@ if selected == "Aplica√ß√£o":
     <style>
     .disclaimer-box {
         background-color: #fff0f5; /* rosa clarinho */
-        border-left: 6px solid #FF1493; /* borda rosa forte √† esquerda */
+        border-left: 6px solid #FF1493; /* borda rosa forte ‡ esquerda */
         padding: 15px 20px;
         border-radius: 8px;
         box-shadow: 2px 2px 8px rgba(255, 20, 147, 0.2);
@@ -300,9 +300,9 @@ if selected == "Aplica√ß√£o":
     </style>
     <div class="disclaimer-box">
         <strong>Disclaimer:</strong><br>
-        As informa√ß√µes contidas neste formul√°rio t√™m car√°ter exclusivamente acad√™mico e s√£o destinadas a fins de pesquisa.
-        N√£o constituem aconselhamento jur√≠dico e s√£o fornecidas como prot√≥tipo, sem garantias de exatid√£o, completude ou atualidade.
-        Os autores n√£o se responsabilizam por eventuais erros ou omiss√µes.
+        As informaÁıes contidas neste formul·rio tÍm car·ter exclusivamente acadÍmico e s„o destinadas a fins de pesquisa.
+        N„o constituem aconselhamento jurÌdico e s„o fornecidas como protÛtipo, sem garantias de exatid„o, completude ou atualidade.
+        Os autores n„o se responsabilizam por eventuais erros ou omissıes.
     </div>
 """, unsafe_allow_html=True)    
 
